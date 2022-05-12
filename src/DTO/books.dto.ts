61fe8ed27558b72c83b165042679977fc06fc075
Daniel Machado <daniel@appmarketing.com.br>
2022-05-12 11:35:54 -0300

    feat: add schemas database and conection database

--- src/DTO/books.dto.ts
+++ src/DTO/books.dto.ts
@@ -2,7 +2,6 @@ import { Type } from 'class-transformer';
 import {
   ArrayMinSize,
   IsNotEmpty,
-  IsNotEmptyObject,
   IsNumber,
   IsPositive,
   IsString,
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { BooksModule } from './Modules/books/books.module';
 
 @Module({
-  imports: [BooksModule],
+  imports: [
+    BooksModule,
+    MongooseModule.forRoot('mongodb://localhost/biblioteca'),
+  ],
   controllers: [],
   providers: [],
 })
